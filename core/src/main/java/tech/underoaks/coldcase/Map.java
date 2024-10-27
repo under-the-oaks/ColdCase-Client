360bd06fa5d7bf0ea0e34c2806acf491a3172cb6
Danmyrer <dangwrb.09@gmail.com>
2024-10-27 22:07:21 +0100

    [7] Added Default method to get empty Map

--- core/src/main/java/tech/underoaks/coldcase/Map.java
+++ core/src/main/java/tech/underoaks/coldcase/Map.java
@@ -3,4 +3,20 @@ package tech.underoaks.coldcase;
 public record Map(
     Tile[][] tileArray
 ) {
+    /**
+     * Generates a new map without any tiles placed
+     *
+     * @param height Height of the map
+     * @param width Width of the map
+     * @return Empty map
+     */
+    public Map getEmptyMap(int height, int width) {
+        Tile[][] tileArray = new Tile[height][width];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                tileArray[y][x] = new VoidTile();
+            }
+        }
+        return new Map(tileArray);
+    }
 }
