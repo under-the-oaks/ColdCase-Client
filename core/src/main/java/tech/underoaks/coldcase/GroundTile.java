95ec6c166902144aabba5f4ac5114167ea5777e2
mabe.edu <Max.Becker@student.hs-rm.de>
2024-10-27 22:15:23 +0100

    ColdCase-Client#7 added ground Tile

---
+++ core/src/main/java/tech/underoaks/coldcase/GroundTile.java
@@ -0,0 +1,23 @@
+package tech.underoaks.coldcase;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GroundTile extends Tile {
+
+    private static final Texture texture = new Texture("./isometric tileset/separated images/tile_063.png");
+
+    public GroundTile(float x, float y) {
+        super(texture, x, y);
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        super.render(batch);
+    }
+
+    
+
+
+
+}
--- core/src/main/java/tech/underoaks/coldcase/Tile.java
+++ core/src/main/java/tech/underoaks/coldcase/Tile.java
@@ -1,4 +1,23 @@
 package tech.underoaks.coldcase;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
 public abstract class Tile {
+
+    private Texture texture;
+    private Vector2 position;
+
+    public Tile(Texture texture, float x, float y) {
+        this.texture = texture;
+        this.position = new Vector2(x, y);
+    }
+
+    public void render(SpriteBatch batch) {
+        if(texture == null) {
+            return;
+        }
+        batch.draw(texture, position.x, position.y);
+    }
 }
