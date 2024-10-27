348879a94a191f387031b5682e49aee9b6b90c28
Danmyrer <dangwrb.09@gmail.com>
2024-10-27 22:30:37 +0100

    [7] Added Default method to get empty Map

--- core/src/main/java/tech/underoaks/coldcase/EmptyTile.java
+++ core/src/main/java/tech/underoaks/coldcase/EmptyTile.java
@@ -1,12 +1,7 @@
 package tech.underoaks.coldcase;
 
-import com.badlogic.gdx.graphics.Texture;
-
 public class EmptyTile extends Tile {
-
-    private static final Texture texture = null;
-
     public EmptyTile() {
-        super(texture);
+        super(null);
     }
 }
--- core/src/main/java/tech/underoaks/coldcase/Map.java
+++ core/src/main/java/tech/underoaks/coldcase/Map.java
@@ -1,5 +1,7 @@
 package tech.underoaks.coldcase;
 
+import java.lang.reflect.InvocationTargetException;
+
 public record Map(
     Tile[][] tileArray
 ) {
@@ -11,10 +13,31 @@ public record Map(
      * @return Empty map
      */
     public Map getEmptyMap(int height, int width) {
+        return getMap(EmptyTile.class, height, width);
+    }
+
+    public Map getPlainMap(int height, int width) {
+        return getMap(GroundTile.class, height, width);
+    }
+
+    /**
+     * Generates a new map that is filled with a specified type of tile
+     *
+     * @param tile Class that will fill the map
+     * @param height Height of the map
+     * @param width Width of the map
+     * @return New Map
+     */
+    private Map getMap(Class<? extends  Tile> tile, int height, int width) {
         Tile[][] tileArray = new Tile[height][width];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                tileArray[y][x] = new EmptyTile();
+                try {
+                    tileArray[y][x] = tile.getDeclaredConstructor().newInstance();
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                         NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
         return new Map(tileArray);
