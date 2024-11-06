12ea174b1f4d58658ef351dda419171708112bd3
JoniBFB <niemals.fuerimmer@gmail.com>
2024-11-06 19:11:11 +0100

    [#28] JUnit hinzugefügt, True-Test hinzugefügt

---
+++ core/src/test/java/tech/underoaks/coldcase/MainTest.java
@@ -0,0 +1,13 @@
+package tech.underoaks.coldcase;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void TrueTest() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+
+}
