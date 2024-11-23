0bd01a15653366c55f6940dfc9d3ffd69b3365e6
JoniBFB <niemals.fuerimmer@gmail.com>
2024-11-23 15:56:03 +0100

    [#52] - Added GloveItem - Modified movableBlock so it can only be pushed with GloveItem -Replaced TestItem1 with GloveItem in the map test_plain

--- core/src/main/java/tech/underoaks/coldcase/state/tileContent/movableBlock.java
+++ core/src/main/java/tech/underoaks/coldcase/state/tileContent/movableBlock.java
@@ -2,6 +2,7 @@ package tech.underoaks.coldcase.state.tileContent;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import tech.underoaks.coldcase.game.PlayerController;
 import tech.underoaks.coldcase.state.updates.GameStateUpdateException;
 import tech.underoaks.coldcase.state.InteractionChain;
 import tech.underoaks.coldcase.state.updates.MoveUpdate;
@@ -20,25 +21,38 @@ public class movableBlock extends TileContent {
     @Override
     public boolean action(InteractionChain chain, Vector2 tilePosition, Direction actionDirection) throws GameStateUpdateException {
 
-        int childIndex = chain.getSnapshot().getSnapshotMap().getChildIndex(tilePosition, this);
+        // No Glove in Inventory
 
-        Vector2 targetPosition = tilePosition.cpy().add(actionDirection.getVector());
-
-        //validation checks: if the target tile is out of bounds
-        Map snapshotMap = chain.getSnapshot().getSnapshotMap();
-        if (snapshotMap.isOutOfBounds(targetPosition)) {
+        if(PlayerController.getInstance().getInventory() == null || PlayerController.getInstance().getInventory().getClass() != GloveItem.class){
+            System.out.println( "Insufficient inventory - Glove needed" );
             return false;
         }
 
-        //validation checks: if the target tile is not passable
-        Tile targetTile = snapshotMap.getTile(targetPosition);
-        TileContent topTargetContent = targetTile.topTileContent();
-        if (topTargetContent != null && !topTargetContent.isObjectPassable()) {
-            return false;
-        }
+        // Glove in Inventory
 
-        chain.addGameStateUpdate(new MoveUpdate(tilePosition, childIndex, targetPosition));
-        return true;
+        else {
+
+            int childIndex = chain.getSnapshot().getSnapshotMap().getChildIndex(tilePosition, this);
+
+            Vector2 targetPosition = tilePosition.cpy().add(actionDirection.getVector());
+
+            //validation checks: if the target tile is out of bounds
+            Map snapshotMap = chain.getSnapshot().getSnapshotMap();
+            if (snapshotMap.isOutOfBounds(targetPosition)) {
+                return false;
+            }
+
+            //validation checks: if the target tile is not passable
+            Tile targetTile = snapshotMap.getTile(targetPosition);
+            TileContent topTargetContent = targetTile.topTileContent();
+            if (topTargetContent != null && !topTargetContent.isObjectPassable()) {
+                return false;
+            }
+
+            chain.addGameStateUpdate(new MoveUpdate(tilePosition, childIndex, targetPosition));
+            return true;
+
+        }
     }
 
     @Override
