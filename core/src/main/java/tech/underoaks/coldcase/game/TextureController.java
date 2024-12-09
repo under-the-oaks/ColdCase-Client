5c68707e7ada73885a33fb116249a4531a140b41
mabe.edu <Max.Becker@student.hs-rm.de>
2024-12-09 18:35:34 +0100

    [#98] fixed portal

--- core/src/main/java/tech/underoaks/coldcase/game/TextureController.java
+++ core/src/main/java/tech/underoaks/coldcase/game/TextureController.java
@@ -1,8 +1,6 @@
 package tech.underoaks.coldcase.game;
 
 import com.badlogic.gdx.graphics.Texture;
-import tech.underoaks.coldcase.state.tileContent.TileContent;
-import tech.underoaks.coldcase.state.tileContent.TileContents;
 
 public class TextureController {
     private static TextureController instance;
@@ -24,13 +22,11 @@ public class TextureController {
 
     private static boolean _isDetective = true;
 
+    Texture gloveTexture = new Texture("./sprites/item_glove_detective_6.png");
+
 
     public Texture getGloveTexture() {
-        return new Texture("./sprites/item_glove_detective_6.png");
-    }
-
-    public Texture getPlayerTexture() {
-        return new Texture(_isDetective ? "./sprites/player_detective.png" : "./sprites/player_sherlock.png");
+        return gloveTexture;
     }
 
 }
