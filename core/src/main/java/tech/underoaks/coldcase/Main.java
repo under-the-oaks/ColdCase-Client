56d7bba944aefa8365eb8b14337053ca1bb4c8a6
jwens001 <jean-luc.wenserski@student.hs-rm.de>
2024-12-09 17:47:34 +0100

    [#98] created map

--- core/src/main/java/tech/underoaks/coldcase/Main.java
+++ core/src/main/java/tech/underoaks/coldcase/Main.java
@@ -31,7 +31,7 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
         viewport = new ExtendViewport(10000, 10000);
 
-        Map map = MapGenerator.serializeContentToMap(Path.of("maps/Map_DoorDemo"), false);
+        Map map = MapGenerator.serializeContentToMap(Path.of("maps/Map_Mvp"), true);
         Gdx.input.setInputProcessor(PlayerController.getInstance());
 
         gameController = GameController.getInstance();
