94547421a64e840a782e3505d4a5039961ffe2a1
Toni Bingenheimer <toni.bingenheimer@student.hs-rm.de>
2025-01-09 17:08:44 +0100

    Revert "Revert "[#133] merged with dev and removed debug printout""
    This reverts commit 464041ae696d789666dff4cc404fa54f7369053e.

--- core/src/main/java/tech/underoaks/coldcase/state/tileContent/Player.java
+++ core/src/main/java/tech/underoaks/coldcase/state/tileContent/Player.java
@@ -80,9 +80,6 @@ public class Player extends TileContent {
         if (tileContent != null) {
             tileContent.render(batch, x, y);
         }
-
-        System.out.println("Player position: " + x + ", " + y);
-
     }
 
     @Override
