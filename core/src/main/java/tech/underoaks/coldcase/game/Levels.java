1b0e8ccf25370bc4cfa681cb57b46c690da39aca
Yassine091 <130560566+Yassine091@users.noreply.github.com>
2025-01-14 09:26:13 +0100

    Update Levels.java
    Die Map test_plain bleibt am besten auskommentiert da es kein GoalObject hat.

--- core/src/main/java/tech/underoaks/coldcase/game/Levels.java
+++ core/src/main/java/tech/underoaks/coldcase/game/Levels.java
@@ -7,12 +7,12 @@ package tech.underoaks.coldcase.game;
  */
 public enum Levels {
     //LEVEL_TEST("maps/test_plain"),
-    //LEVEL_01("maps/Map_GoalDemo"),
-    //LEVEL_02("maps/Map_Mvp"),
-   //LEVEL_03("maps/New_Level_Test_Medium"),
-   //LEVEL_04("maps/New_Level_Test_Hard"),
-   // LEVEL_05("maps/Map_Tutorial_normalBlock"),
-   // LEVEL_06("maps/Map_Tutorial_transcendentBlock"),
+    LEVEL_01("maps/Map_GoalDemo"),
+    LEVEL_02("maps/Map_Mvp"),
+    LEVEL_03("maps/New_Level_Test_Medium"),
+    LEVEL_04("maps/New_Level_Test_Hard"),
+    LEVEL_05("maps/Map_Tutorial_normalBlock"),
+    LEVEL_06("maps/Map_Tutorial_transcendentBlock"),
     LEVEL_07("maps/Map_Tutorial_spikes");
 
     private final String mapPath;
